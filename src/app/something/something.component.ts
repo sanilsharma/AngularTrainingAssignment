80689f49f216a700c7d6f3f84c7595401c233f75
Sanil Sharma <sanil.sharma@BRILLIO.COM>
2020-05-03 17:19:19 +0530

    minor ui changes

--- src/app/something/something.component.ts
+++ src/app/something/something.component.ts
@@ -12,6 +12,7 @@ export class SomethingComponent implements OnInit {
   model = new Customer('', 0);
   update = false;
   indexToUpdate = undefined;
+  show = this.custArray.length === 0 ? false : true;
 
   constructor() { }
 
@@ -30,10 +31,12 @@ export class SomethingComponent implements OnInit {
     this.custArray.push(new Customer(this.model.name, this.model.age));
     console.log(JSON.stringify(this.custArray));
     this.model = new Customer('', 0);
+    this.show = this.custArray.length === 0 ? false : true;
   }
 
   deleteCustomer(i: number) {
     this.custArray.splice(i, 1);
+    this.show = this.custArray.length === 0 ? false : true;
   }
 
   getCustomerForUpdate(i: number) {
@@ -46,6 +49,7 @@ export class SomethingComponent implements OnInit {
     this.custArray[this.indexToUpdate].name = this.model.name;
     this.custArray[this.indexToUpdate].age = this.model.age;
     console.log('item updated');
+    this.show = this.custArray.length === 0 ? false : true;
     this.update = false;
     this.indexToUpdate = undefined;
     this.model = new Customer('', 0);
